39925a8d523724b3d9554f239db2051aac39ee1d
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-10-05 00:30:13 +0900

    add

--- src/train.py
+++ src/train.py
@@ -2,7 +2,12 @@ import argparse
 
 from dataset import PetfinderDataModule
 from utils.factory import get_transform, read_yaml
-
+from sklearn.model_selection import StratifiedKFold
+from pytorch_lightning.utilities.seed import seed_everything
+from pytorch_lightning import callbacks
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks.ealy_stopping import EarlyStopping
 
 def make_parse():
     parser = argparse.ArgumentParser()
@@ -15,13 +20,16 @@ def make_parse():
 def main():
     args = make_parse().parse_args()
     conf = read_yaml(fpath=args.config)
-    print(conf.keys())
-    print(type(conf))
 
     conf_aug = conf.Augmentation["train"]
     transform = get_transform(conf_aug)
 
+    # 下の行わからん
+    torch.autograd.set_detect_anomaly(True)
+    seed_everything(conf.General.seed)
+
     # df読み込む
+    
 
     skf = StratifiedKFold(
         n_splits=conf.dataset.kfold, shuffle=True, random_state=conf.General.seed
@@ -29,6 +37,7 @@ def main():
 
     for fold, (train_idx, val_idx) in enumerate(skf.split(df["Id"], df["Pawpularity"])):
         train_df = df.loc[train_idx].reset_index(drop=True)
+
         val_df = df.loc[val_idx].reset_index(drop=True)
         datamodule = PetfinderDataModule(train_df, val_df, conf)
         model = Model(conf)
