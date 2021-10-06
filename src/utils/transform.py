4d7f421becf3a452714a1770de4ca9fab6ff9e41
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-10-06 17:25:04 +0900

    add transforms

---
+++ src/utils/transform.py
@@ -0,0 +1,25 @@
+import torch
+import torchvision.transforms as T
+
+def get_default_transforms():
+    IMAGENET_MEAN = [0.485, 0.456, 0.406]  # RGB
+    IMAGENET_STD = [0.229, 0.224, 0.225]  # RGB
+    transform = {
+        "train": T.Compose(
+            [
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+                T.RandomAffine(15, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+                T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+                T.ConvertImageDtype(torch.float),
+                T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+            ]
+        ),
+        "valid": T.Compose(
+            [
+                T.ConvertImageDtype(torch.float),
+                T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+            ]
+        ),
+    }
+    return transform
