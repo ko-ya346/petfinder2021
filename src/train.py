c706833a85917db490ea14b0179b9f1188443888
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-10-08 10:24:45 +0900

    move import , formater

--- src/train.py
+++ src/train.py
@@ -1,48 +1,51 @@
 import argparse
-
-import warnings
 import os
+import warnings
+
 import pandas as pd
 import torch
 from dataset import PetfinderDataModule
-from utils.factory import get_transform, read_yaml
-from sklearn.model_selection import StratifiedKFold
-from pytorch_lightning.utilities.seed import seed_everything
-from pytorch_lightning import callbacks
-import pytorch_lightning as pl
-from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
-
 from model import Model
+from sklearn.model_selection import StratifiedKFold
+from utils.factory import get_transform, read_yaml
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
+
 
 def make_parse():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     arg("--debug", action="store_true", help="debug")
+    arg("--knotebook", action="store_true", help="run kaggle notebook")
     arg("--config", default=None, type=str, help="config path")
     return parser
 
 
 def main():
+    import pytorch_lightning as pl
+    from pytorch_lightning import callbacks
+    from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+    from pytorch_lightning.loggers import TensorBoardLogger
+    from pytorch_lightning.utilities.seed import seed_everything
+
     args = make_parse().parse_args()
     conf = read_yaml(fpath=args.config)
 
-    if conf.General.debug:
+    if args.debug:
         conf.General.epoch = 3
 
     conf_aug = conf.Augmentation["train"]
     transform = get_transform(conf_aug)
 
-    # 下の行わからん
+    # BackwardテンソルのNaNチェックを行う
     torch.autograd.set_detect_anomaly(True)
     seed_everything(conf.General.seed)
 
     # df読み込む
-    df = pd.read_csv(os.path.join(conf.dataset.train_df, 'train.csv'))
-    df['Id'] = df['Id'].apply(lambda x: os.path.join(conf.dataset.train_img_dir, x+'.jpg'))
-
+    df = pd.read_csv(os.path.join(conf.dataset.train_df, "train.csv"))
+    df["Id"] = df["Id"].apply(
+        lambda x: os.path.join(conf.dataset.train_img_dir, x + ".jpg")
+    )
 
     skf = StratifiedKFold(
         n_splits=conf.dataset.kfold, shuffle=True, random_state=conf.General.seed
@@ -65,7 +68,9 @@ def main():
             dirpath=os.path.join(conf.General.output_dir, conf.General.name),
         )
 
-        logger = TensorBoardLogger(os.path.join(conf.General.output_dir, conf.General.name))
+        logger = TensorBoardLogger(
+            os.path.join(conf.General.output_dir, conf.General.name)
+        )
         trainer = pl.Trainer(
             logger=logger,
             max_epochs=conf.General.epoch,
