f5e20d5111e9f79b52777dfbf6a18f1df1494db3
kouya takahashi <3kamarin4skaya6@gmail.com>
2021-10-13 22:35:01 +0900

    [add] meta data

--- src/dataset.py
+++ src/dataset.py
@@ -5,9 +5,13 @@ from torchvision.io import read_image
 
 
 class PetfinderDataset(Dataset):
-    def __init__(self, df, img_height=256, img_width=256):
+    def __init__(self, df, feature_cols=[], img_height=256, img_width=256):
         self._X = df["Id"].values
         self._y = None
+        self._features = None
+        if len(feature_cols):
+            self._features = df[feature_cols].values
+
         if "Pawpularity" in df.keys():
             self._y = df["Pawpularity"].values
         self._transform = T.Resize([img_height, img_width])
@@ -19,29 +23,40 @@ class PetfinderDataset(Dataset):
         image_path = self._X[idx]
         image = read_image(image_path)
         image = self._transform(image)
-        if self._y is not None:
-            label = self._y[idx]
-            return image, label
-        return image
+        if self._features is None: 
+            if self._y is not None:
+                label = self._y[idx]
+                return image, label
+            return image
+        else: 
+            features = self._features[idx]
+            if self._y is not None:
+                label = self._y[idx]
+                return image, features, label
+            return image, features
 
 
 class PetfinderDataModule(LightningDataModule):
-    def __init__(self, train_df, val_df, conf):
+    def __init__(self, train_df, val_df, conf, feature_cols=[]):
         super().__init__()
         self._train_df = train_df
         self._val_df = val_df
         self._conf = conf
+        self.feature_cols = feature_cols
 
     def __create_dataset(self, train=True):
         return (
             PetfinderDataset(
                 self._train_df,
+                self.feature_cols,
                 self._conf.dataset.img_height,
                 self._conf.dataset.img_width,
             )
             if train
             else PetfinderDataset(
                 self._val_df,
+                self.feature_cols,
+                self._conf.dataset.img_height,
                 self._conf.dataset.img_height,
                 self._conf.dataset.img_width,
             )
